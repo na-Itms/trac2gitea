8e663b3da5d6b4350ac5b0332a26457e53d02914
na-Itms <na.itms76@gmail.com>
2024-04-10 23:07:38 +0200

    Improved conversion of Phab links

--- markdown/wfg.go
+++ markdown/wfg.go
@@ -2,9 +2,16 @@ package markdown
 
 import "regexp"
 
-// regexp for WFG's Phabricator links
-var phabricatorLinkRegexp = regexp.MustCompile(`Phab:(D[[:digit:]]+)`)
+// regexp for WFG's Phabricator links to diffs, files or pastes
+var phabricatorLinkRegexp = regexp.MustCompile(`[Pp]hab:([DFP][[:digit:]]+)`)
+
+// regexp for WFG's Phabricator links to revisions
+var phabricatorRevRegexp = regexp.MustCompile(`[Pp]hab:rP([[:digit:]]+)`)
 
 func (converter *DefaultConverter) convertPhabLinks(in string) string {
-	return phabricatorLinkRegexp.ReplaceAllString(in, "[$0](https://code.wildfiregames.com/$1)")
+	out := phabricatorLinkRegexp.ReplaceAllString(in, "[$0](https://code.wildfiregames.com/$1)")
+
+	out = phabricatorRevRegexp.ReplaceAllString(out, "r$1")
+
+	return out
 }
